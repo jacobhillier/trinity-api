95d34383b58c1410b4334bf539f2a0a9c517b307
Jacob <jacobhillier83@gmail.com>
2014-12-27 11:28:29 -0600

    transforms multi-level associations

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ var express = require('express');
 var mongo = require('mongodb');
 var assert = require('assert');
 var array = require('array');
+var log = require('loglevel');
 
 var mongoUrl = 'mongodb://trinity-cms:tr1n1ty@dbh44.mongolab.com:27447/trinity';
 var MongoClient = mongo.MongoClient;
 var ObjectId = mongo.ObjectId;
+
+log.setLevel('info');
 
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/api/v1/:contentTypeUri/:contentId', function (request, response) {
     var findContent = function (id, transformType, failOnNotFound, db, callback) {
-        // console.log('Finding content [%s]', id);
+        log.debug('findContent [%s], [%s], [%s]', id, transformType, failOnNotFound);
+
         var collection = db.collection('content');
 
         collection.findOne({_id: ObjectId(id)}, function (err, content) {
@@ -27,6 +31,8 @@ app.get('/api/v1/:contentTypeUri/:contentId', function (request, response) {
     };
 
     var transformContent = function (content, transformType, db, callback) {
+        log.debug('transformContent [%j], [%s]', content, transformType);
+
         findContentType(content.type, db, function (contentType) {
                 var transformedContent = {
                     id: content._id,
@@ -42,7 +48,7 @@ app.get('/api/v1/:contentTypeUri/:contentId', function (request, response) {
                     }
                 });
 
-                callback(content, contentType, transformedContent, transformType);
+                callback(content, transformedContent, contentType, transformType);
             }
         )
         ;
@@ -61,7 +67,8 @@ app.get('/api/v1/:contentTypeUri/:contentId', function (request, response) {
     };
 
     var findContentType = function (id, db, callback) {
-        // console.log('Finding content type [%s]', id);
+        log.debug('findContentType [%s]', id);
+
         var collection = db.collection('contentType');
 
         collection.findOne({_id: ObjectId(id)}, function (err, contentType) {
@@ -71,17 +78,23 @@ app.get('/api/v1/:contentTypeUri/:contentId', function (request, response) {
         });
     };
 
-    var findAssociatedContent = function (content, contentType, transformedContent, transformType, db, callback) {
+    var findAssociatedContent = function (content, transformedContent, contentType, transformType, db, callback) {
+        log.debug('findAssociatedContent [%j], [%j]', content, transformType);
+
         var contentIds = extractUniqueAssociationIds(content, contentType, transformType);
         if (contentIds.length > 0) {
-            var transformedAssociatedContent = [];
+            var associatedTransformedContents = [];
             contentIds.each(function (id) {
-                findContent(id, 'apiTeaser', false, db, function (associatedContent, type) {
-                    transformContent(associatedContent, type, db, function (associatedContent, associatedContentType, associatedTransformedContent) {
-                        transformedAssociatedContent.push(associatedTransformedContent);
-                        if (transformedAssociatedContent.length == contentIds.length) {
-                            populateAssociatedContent(content, transformedContent, contentType, transformedAssociatedContent, transformType, callback);
-                        }
+                findContent(id, 'apiTeaser', false, db, function (associatedContent, associatedTransformType) {
+                    transformContent(associatedContent, associatedTransformType, db, function (associatedContent, associatedTransformedContent, associatedContentType) {
+                        findAssociatedContent(associatedContent, associatedTransformedContent, associatedContentType, associatedTransformType, db, function (associatedTransformedContent) {
+                            associatedTransformedContents.push(associatedTransformedContent);
+                            if (associatedTransformedContents.length == contentIds.length) {
+                                populateAssociatedContent(content, transformedContent, contentType, associatedTransformedContents, transformType);
+
+                                callback(transformedContent);
+                            }
+                        });
                     });
                 });
             });
@@ -91,10 +104,12 @@ app.get('/api/v1/:contentTypeUri/:contentId', function (request, response) {
     };
 
     var extractUniqueAssociationIds = function (content, contentType, transformType) {
+        log.debug('extractUniqueAssociationIds [%j], [%j], [%s]', content, contentType, transformType);
+
         var contentIds = array();
 
         array(contentType.fields).each(function (field) {
-            if (field[transformType]) {
+            if (field[transformType] && content.fields[field._id]) {
                 if (isAssociationField(field)) {
                     contentIds.push(content.fields[field._id]._id);
                 } else if (isListAssociationField(field)) {
@@ -108,10 +123,12 @@ app.get('/api/v1/:contentTypeUri/:contentId', function (request, response) {
         return contentIds.unique();
     };
 
-    var populateAssociatedContent = function (content, transformedContent, contentType, transformedAssociatedContent, transformType, callback) {
+    var populateAssociatedContent = function (content, transformedContent, contentType, transformedAssociatedContent, transformType) {
+        log.debug('populateAssociatedContent [%j], [%j], [%j], [%j], [%s]', content, transformedContent, contentType, transformedAssociatedContent, transformType);
+
         var associatedContentForId = function (id) {
             return array(transformedAssociatedContent).find(function (transformedAssociatedContent) {
-                return transformedAssociatedContent.id == id.toHexString();
+                return id && transformedAssociatedContent.id == id.toHexString();
             });
         };
 
@@ -135,8 +152,6 @@ app.get('/api/v1/:contentTypeUri/:contentId', function (request, response) {
                 }
             }
         });
-
-        callback(transformedContent);
     };
 
     var sendNotFound = function (db) {
@@ -149,8 +164,8 @@ app.get('/api/v1/:contentTypeUri/:contentId', function (request, response) {
         assert.equal(null, err);
 
         findContent(request.params.contentId, 'apiFull', true, db, function (content, transformType) {
-            transformContent(content, transformType, db, function (content, contentType, transformedContent, transformType) {
-                findAssociatedContent(content, contentType, transformedContent, transformType, db, function (transformedContent) {
+            transformContent(content, transformType, db, function (content, transformedContent, contentType, transformType) {
+                findAssociatedContent(content, transformedContent, contentType, transformType, db, function (transformedContent) {
                     response.send(transformedContent);
                     db.close();
                 });
